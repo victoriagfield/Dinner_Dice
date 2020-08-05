52c3400afc71755d4037becf94495e74762b5ac8
Sean Cummings <seanpc1028@gmail.com>
2020-08-04 20:51:40 -0400

    radio checks

--- assets/scripts/dinnerdice.js
+++ assets/scripts/dinnerdice.js
@@ -1,41 +1,75 @@
-var drinkOpt1 = document.querySelector(".radioVodka");
-var drinkOpt2 = document.querySelector(".radioWhiskey");
-var drinkOpt3 = document.querySelector(".radioTequila");
-var drinkOpt4 = document.querySelector(".radioRum");
-var drinkOpt5 = document.querySelector(".radioGin");
+var drinkVodka = document.querySelector("#radioVodka");
+var drinkWhiskey = document.querySelector("#radioBrandy");
+var drinkTequlia = document.querySelector("#radioTequila");
+var drinkRum = document.querySelector("#radioRum");
+var drinkGin = document.querySelector("#radioGin");
 
-var mealOpt1 = document.querySelector(".radioChicken");
-var mealOpt2 = document.querySelector(".radioBeef");
-var mealOpt3 = document.querySelector(".radioSeafood");
-var mealOpt4 = document.querySelector(".radioVegan");
-var mealOpt5 = document.querySelector(".radioVegetarian");
-
-
-var foodBtn = document.querySelector(".btnFood");
-var drinkBtn = document.querySelector(".btnDrink");
+var mealChicken = document.querySelector("#radioChicken");
+var mealBeef = document.querySelector("#radioBeef");
+var mealSeafood = document.querySelector("#radioSeafood");
+var mealVegan = document.querySelector("#radioVegan");
+var mealVegetarian = document.querySelector("#radioVegetarian");
 
 
-foodBtn.addEventListener("click", function(){
-    var option = $(this).attr("data-name");
+var foodBtn = document.querySelector("#btnFood");
+var drinkBtn = document.querySelector("#btnDrink");
+
+
+drinkBtn.addEventListener("click", function(){
+    var option = "";
+    if(drinkVodka.checked){
+        option = "Vodka";
+    }
+    else if(drinkBrandy.checked){
+        option = "Brandy";
+    }
+    else if(drinkTequlia.checked){
+        option = "Tequila";
+    }
+    else if(drinkRum.checked){
+        option = "Rum";
+    }
+    else if(drinkGin.checked){
+        option = "Gin";
+    }
+    console.log(option)
+
     var queryURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + option;
 
-// Creating an AJAX call for the specific movie button being clicked
+// Creating an AJAX call for a list of specified alcohol.
     $.ajax({
           url: queryURL,
           method: "GET"
         }).then(function(response) {
-            var drinkID = response.idDrink;
+            var drinkID = response[(Math.floor(Math.random() * response.length))].idDrink;
             var drinkName = response.strDrink;
-
+            console.log = drinkID;
         });
 })
 
 
-drinkBtn.addEventListener("click", function(){
-    var option = $(this).attr("data-name");
+foodBtn.addEventListener("click", function(){
+    var option = "";
+    if(mealChicken.checked){
+        option = "Chicken";
+    }
+    else if(mealBeef.checked){
+        option = "Beef";
+    }
+    else if(mealSeafood.checked){
+        option = "Seafood";
+    }
+    else if(mealVegan.checked){
+        option = "Vegan";
+    }
+    else if(mealVegetarian.checked){
+        option = "Vegetarian";
+    }
+    console.log(option);
+    
     var queryURL = "https://www.themealdb.com/api/json/v2/9973533/filter.php?c=" + option;
 
-// Creating an AJAX call for the specific movie button being clicked
+// Creating an AJAX call for a list of random meals with the specified catergory.
     $.ajax({
           url: queryURL,
           method: "GET"
