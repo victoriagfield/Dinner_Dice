20dcf45d438505f12f5c1e387364ddffc69fbeb0
Sean Cummings <seanpc1028@gmail.com>
2020-08-05 19:33:48 -0400

    first working version

--- assets/scripts/dinnerdice.js
+++ assets/scripts/dinnerdice.js
@@ -1,12 +1,12 @@
 var drinkVodka = document.querySelector("#radioVodka");
-var drinkWhiskey = document.querySelector("#radioBrandy");
+var drinkBrandy = document.querySelector("#radioBrandy");
 var drinkTequlia = document.querySelector("#radioTequila");
 var drinkRum = document.querySelector("#radioRum");
 var drinkGin = document.querySelector("#radioGin");
 
 var mealChicken = document.querySelector("#radioChicken");
 var mealBeef = document.querySelector("#radioBeef");
-var mealSeafood = document.querySelector("#radioSeafood");
+var mealPork = document.querySelector("#radioPork");
 var mealVegan = document.querySelector("#radioVegan");
 var mealVegetarian = document.querySelector("#radioVegetarian");
 
@@ -14,9 +14,11 @@ var mealVegetarian = document.querySelector("#radioVegetarian");
 var foodBtn = document.querySelector("#btnFood");
 var drinkBtn = document.querySelector("#btnDrink");
 
+var drinkResults = document.querySelector("#drinkResults");
 
+var option = "";
 drinkBtn.addEventListener("click", function(){
-    var option = "";
+    
     if(drinkVodka.checked){
         option = "Vodka";
     }
@@ -32,19 +34,28 @@ drinkBtn.addEventListener("click", function(){
     else if(drinkGin.checked){
         option = "Gin";
     }
-    console.log(option)
+    console.log(option);
 
     var queryURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + option;
-
+    console.log(queryURL);
 // Creating an AJAX call for a list of specified alcohol.
     $.ajax({
           url: queryURL,
           method: "GET"
         }).then(function(response) {
-            var drinkID = response[(Math.floor(Math.random() * response.length))].idDrink;
-            var drinkName = response.strDrink;
-            console.log = drinkID;
+            var randomIndex = Math.floor(Math.random() * response.drinks.length)
+            var drinkName = response.drinks[randomIndex].strDrink;
+            console.log(drinkName);
+            $.ajax({
+                url: "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=" + drinkName,
+                method: "GET"
+              }).then(function(response) {//response.drinks.strDrinkThumb,
+                $("#drinkResults").html("<figure> <img src = " + response.drinks[0].strDrinkThumb + "/preview" + ">" + "<figcaption>" + drinkName + "</figcaption> </figure>");
+                console.log(this);
+                
+              });
         });
+        
 })
 
 
@@ -57,7 +68,7 @@ foodBtn.addEventListener("click", function(){
         option = "Beef";
     }
     else if(mealSeafood.checked){
-        option = "Seafood";
+        option = "Pork";
     }
     else if(mealVegan.checked){
         option = "Vegan";
@@ -68,14 +79,22 @@ foodBtn.addEventListener("click", function(){
     console.log(option);
     
     var queryURL = "https://www.themealdb.com/api/json/v2/9973533/filter.php?c=" + option;
-
+    console.log(queryURL);
 // Creating an AJAX call for a list of random meals with the specified catergory.
     $.ajax({
           url: queryURL,
           method: "GET"
         }).then(function(response) {
-            var mealID = response.idMeal;
-            var mealName = response.strMeal;
+            var randomIndex = Math.floor(Math.random() * response.meals.length)
+            var mealName = response.meals[randomIndex].strMeal;
+            $.ajax({
+                url: "https://www.themealdb.com/api/json/v2/9973533/search.php?s=" + mealName,
+                method: "GET"
+              }).then(function(response) {
+                $("#recipeResults").html("<figure> <img src = " + response.meals[0].strMealThumb + "/preview" + ">" + "<figcaption>" + mealName + "</figcaption> </figure>");
+                console.log(this);
+                
+              });
 
         });
 })
