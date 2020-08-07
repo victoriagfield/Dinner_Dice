d07165f3d7f0b0b4f5ab31d84378d94fec26ab74
Victoria Greenfield <victoriagfield@gmail.com>
2020-08-06 20:03:04 -0400

    modal pop-up fix

--- assets/scripts/dinnerdice.js
+++ assets/scripts/dinnerdice.js
@@ -17,90 +17,100 @@ var drinkBtn = document.querySelector("#btnDrink");
 var drinkResults = document.querySelector("#drinkResults");
 
 var option = "";
-drinkBtn.addEventListener("click", function(){
-    
-    if(drinkVodka.checked){
+drinkBtn.addEventListener("click", function () {
+
+    if (drinkVodka.checked) {
         option = "Vodka";
     }
-    else if(drinkBrandy.checked){
+    else if (drinkBrandy.checked) {
         option = "Brandy";
     }
-    else if(drinkTequlia.checked){
+    else if (drinkTequlia.checked) {
         option = "Tequila";
     }
-    else if(drinkRum.checked){
+    else if (drinkRum.checked) {
         option = "Rum";
     }
-    else if(drinkGin.checked){
+    else if (drinkGin.checked) {
         option = "Gin";
     }
     console.log(option);
 
     var queryURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + option;
     console.log(queryURL);
-// Creating an AJAX call for a list of specified alcohol.
+    // Creating an AJAX call for a list of specified alcohol.
     $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-            var randomIndex = Math.floor(Math.random() * response.drinks.length)
-            var drinkName = response.drinks[randomIndex].strDrink;
-            console.log(drinkName);
-            $.ajax({
-                url: "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=" + drinkName,
-                method: "GET"
-              }).then(function(data) {//response.drinks.strDrinkThumb,
-                $("#drinkResults").html("<figure> <img src = " + data.drinks[0].strDrinkThumb + "/preview" + ">" + "<figcaption>" + drinkName + "</figcaption> </figure> <br> <button id = 'ingredBtn'> Click Here for Ingredients </button>");
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var randomIndex = Math.floor(Math.random() * response.drinks.length)
+        var drinkName = response.drinks[randomIndex].strDrink;
+        console.log(drinkName);
+        $.ajax({
+            url: "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=" + drinkName,
+            method: "GET"
+        }).then(function (data) {//response.drinks.strDrinkThumb,
+            $("#drinkResults").html("<figure> <img src = " + data.drinks[0].strDrinkThumb + "/preview" + ">" + "<figcaption>" + drinkName + "</figcaption> </figure>");
+            var btnView = $('<button>').addClass('btn btn-sm btn-primary');
+            btnView.text("Click to see the recipe");
+            btnView.attr('data-toggle', 'modal');
+            btnView.attr('data-target', '#recipeModal');
+            $("#drinkResults").append(btnView);
 
-                ingredCall();
-                
-                
-              });
+            $('#recipeModal').on('show.bs.modal', function (event) {
+                var button = $(event.relatedTarget);
+                $("#modal-body").text(response.drinks[0].strInstructions);
+            });
         });
-        
-})
-$("#ingredBtn").on('click', function ingredCall(){
+    });
+});
 
-                    $("#recipeModal").html();
-                    $("#modal-body").html(response.strInstructions)
-                })
-
-foodBtn.addEventListener("click", function(){
+foodBtn.addEventListener("click", function () {
     var option = "";
-    if(mealChicken.checked){
+    if (mealChicken.checked) {
         option = "Chicken";
     }
-    else if(mealBeef.checked){
+    else if (mealBeef.checked) {
         option = "Beef";
     }
-    else if(mealPork.checked){
+    else if (mealPork.checked) {
         option = "Pork";
     }
-    else if(mealVegan.checked){
+    else if (mealVegan.checked) {
         option = "Vegan";
     }
-    else if(mealVegetarian.checked){
+    else if (mealVegetarian.checked) {
         option = "Vegetarian";
     }
     console.log(option);
-    
+
     var queryURL = "https://www.themealdb.com/api/json/v2/9973533/filter.php?c=" + option;
     console.log(queryURL);
-// Creating an AJAX call for a list of random meals with the specified catergory.
+    // Creating an AJAX call for a list of random meals with the specified catergory.
     $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-            var randomIndex = Math.floor(Math.random() * response.meals.length)
-            var mealName = response.meals[randomIndex].strMeal;
-            $.ajax({
-                url: "https://www.themealdb.com/api/json/v2/9973533/search.php?s=" + mealName,
-                method: "GET"
-              }).then(function(response) {
-                $("#recipeResults").html("<figure> <img src = " + response.meals[0].strMealThumb + "/preview" + ">" + "<figcaption>" + mealName + "</figcaption> </figure>");
-                console.log(this);
-                
-              });
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var randomIndex = Math.floor(Math.random() * response.meals.length)
+        var mealName = response.meals[randomIndex].strMeal;
+        $.ajax({
+            url: "https://www.themealdb.com/api/json/v2/9973533/search.php?s=" + mealName,
+            method: "GET"
+        }).then(function (response) {
+            $("#recipeResults").html("<figure> <img src = " + response.meals[0].strMealThumb + "/preview" + ">" + "<figcaption>" + mealName + "</figcaption> </figure>");
+            console.log(this);
+            var btnView = $('<button>').addClass('btn btn-sm btn-primary');
+            btnView.text("Click to see the recipe");
+            btnView.attr('data-toggle', 'modal');
+            btnView.attr('data-target', '#recipeModal');
+            $("#recipeResults").append(btnView);
+
+            $('#recipeModal').on('show.bs.modal', function (event) {
+                var button = $(event.relatedTarget);
+                $("#modalText").text(response.meals[0].strInstructions);
+            });
 
         });
+
+    });
 })
