5a68e3a1cb46ae03804fd917cfa9f5446148b7f3
Victoria Greenfield <victoriagfield@gmail.com>
2020-08-08 15:21:01 -0400

    adding comments

--- assets/scripts/dinnerdice.js
+++ assets/scripts/dinnerdice.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
+
+//Global drink variables - attaches to the radio buttons
 var drinkVodka = document.querySelector("#radioVodka");
 var drinkBrandy = document.querySelector("#radioBrandy");
 var drinkTequlia = document.querySelector("#radioTequila");
 var drinkRum = document.querySelector("#radioRum");
 var drinkGin = document.querySelector("#radioGin");
 
+//Global food variables - attaches to the radio buttons
 var mealChicken = document.querySelector("#radioChicken");
 var mealBeef = document.querySelector("#radioBeef");
 var mealPork = document.querySelector("#radioPork");
@@ -12,13 +15,14 @@ var mealVegan = document.querySelector("#radioVegan");
 var mealVegetarian = document.querySelector("#radioVegetarian");
 
 
-
+//variables for the food and drink buttons
 var foodBtn = document.querySelector("#btnFood");
 var drinkBtn = document.querySelector("#btnDrink");
 
 var option = "";
-drinkBtn.addEventListener("click", function () {
 
+drinkBtn.addEventListener("click", function () {
+//conditionals for whichever drink option the user picks
     if (drinkVodka.checked) {
         option = "Vodka";
     }
@@ -36,9 +40,10 @@ drinkBtn.addEventListener("click", function () {
     }
     console.log(option);
     
-
+//API link for the alcoholic beverages
     var queryURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + option;
     console.log(queryURL);
+
     // Creating an AJAX call for a list of specified alcohol.
     $.ajax({
         url: queryURL,
@@ -50,18 +55,17 @@ drinkBtn.addEventListener("click", function () {
         $.ajax({
             url: "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=" + drinkName,
             method: "GET"
-            //url: "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=" + "Kool-Aid%20Shot"//
-        }).then(function (data) {//response.drinks.strDrinkThumb,
+        }).then(function (data) {
             $("#drinkResults").html("<figure> <img src = " + data.drinks[0].strDrinkThumb + "/preview" + ">" + "<figcaption>" + drinkName + "</figcaption> </figure>");
-            var btnView = $('<button>').addClass('btn btn-sm btn-primary');
-            btnView.text("Click to see the recipe");
+            var btnView = $('<button>').addClass('btn btn-sm btn-primary'); //creating the open button in the results window
+            btnView.text("Click to see the recipe"); //changing the text of the button
             btnView.attr('data-toggle', 'modal');
-            btnView.attr('data-target', '#recipeModal');
+            btnView.attr('data-target', '#recipeModal'); //triggers the modal
             $("#drinkResults").append(btnView);
             btnView.on("click", function () {
             $('#recipeModal').on('show.bs.modal', function (event) {
                 var button = $(event.relatedTarget);
-                $("#modalText").text(data.drinks[0].strInstructions);
+                $("#modalText").text(data.drinks[0].strInstructions); //getting the recipe instruction and ingredients into the modal
                 $("#ingredientsText").text(" ");
                 for(var i = 1; i <= 15; i++){
 
@@ -72,19 +76,21 @@ drinkBtn.addEventListener("click", function () {
                         break;
                      
                     }
-                    //Our Problem
+                    //removing the word "null" from the API and replacing it with our own text 
                     if((drinkM == null || drinkM == "") && drinkI.length > 0){
                         drinkM = "Dealers choice of";
                     }
+                    //removing the word null when there are no other ingredients that need to be listed in the recipe
                     if((drinkM == null) && drinkI.length == 0){
                         drinkM = "";
                     }
-                    $("#ingredientsText").append(drinkM + ' ' + drinkI);
+                    $("#ingredientsText").append(drinkM + ' ' + drinkI); //adding the text for the instructions and ingredients into the modal
                     $("#ingredientsText").append("<br>");
 
                     
                 }
             });
+            //copy to clipboard function
             $("#copy-btn").click(function() {
                 var concat = $("#modalText").text() + " " + $("#ingredientsText").text();
                 var textarea = $("<textarea>");
@@ -103,6 +109,7 @@ drinkBtn.addEventListener("click", function () {
 })
 
 foodBtn.addEventListener("click", function () {
+    //conditionals for whichever food option the user picks
     var option = "";
     if (mealChicken.checked) {
         option = "Chicken";
@@ -121,6 +128,7 @@ foodBtn.addEventListener("click", function () {
     }
     console.log(option);
 
+    //API link for the food options
     var queryURL = "https://www.themealdb.com/api/json/v2/9973533/filter.php?c=" + option;
     console.log(queryURL);
     // Creating an AJAX call for a list of random meals with the specified catergory.
@@ -136,14 +144,15 @@ foodBtn.addEventListener("click", function () {
             method: "GET"
         }).then(function (response) {
             $("#recipeResults").html("<figure> <img src = " + response.meals[0].strMealThumb + "/preview" + ">" + "<figcaption>" + mealName + "</figcaption> </figure>");
-            //console.log(this);
-            var btnView = $('<button>').addClass('btn btn-sm btn-primary');
-            btnView.text("Click to see the recipe");
+            
+            //button for the result card
+            var btnView = $('<button>').addClass('btn btn-sm btn-primary'); //creating the open button in the results window
+            btnView.text("Click to see the recipe"); //changing the text of the button
             btnView.attr('data-toggle', 'modal');
-            btnView.attr('data-target', '#recipeModal');
+            btnView.attr('data-target', '#recipeModal'); //triggers the modal
             $("#recipeResults").append(btnView);
             btnView.on("click", function () {
-            $('#recipeModal').on('show.bs.modal', function (event) {
+            $('#recipeModal').on('show.bs.modal', function (event) { //getting the recipe instruction and ingredients into the modal
                 var button = $(event.relatedTarget);
                 $("#modalText").text(response.meals[0].strInstructions);
                 $("#ingredientsText").text(" ");
@@ -156,18 +165,20 @@ foodBtn.addEventListener("click", function () {
                         break;
                      
                     }
-                    //Our Problem
+                    //removing the word "null" from the API and replacing it with our own text
                     if((mealM == null || mealM == "") && mealI.length > 0){
                         mealM = "Dealers choice of";
                     }
+                    //removing the word null when there are no other ingredients that need to be listed in the recipe
                     if((mealM == null) && mealI.length == 0){
                         mealM = "";
                     }
-                    $("#ingredientsText").append(mealM + ' ' + mealI);
+                    $("#ingredientsText").append(mealM + ' ' + mealI); //adding the text for the instructions and ingredients into the modal
                     $("#ingredientsText").append("<br>");
                 }
                 
             });
+            //Copy to clipboard function
             $("#copy-btn").click(function() {
                 var concat = $("#modalText").text() + " " + $("#ingredientsText").text();
                 var textarea = $("<textarea>");
